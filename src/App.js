99b2c37469e2b3ddfe69b5d2ce1ea762e40ea221
sundarrajandeveloper <guru3sundar@gmail.com>
2022-08-07 22:25:13 +0530

    six

--- src/App.js
+++ src/App.js
@@ -4,18 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
-     
         
-        <a
-          className="App-link"
-          href="https://portal.azure.com/#@sivasundar0003gmail.onmicrosoft.com/resource/subscriptions/788f9231-4ad7-412d-a8ce-8e16bdab7e9c/resourcegroups/s/providers/Microsoft.Web/staticSites/ss/staticsite"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          welcome sundarrajan jobs
-        </a>
-      
+       
 
      <p> welcome sundarrajan </p>
 
